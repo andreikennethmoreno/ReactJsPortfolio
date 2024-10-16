f67902075c52c0f4c978ffa4bf112ba976a5a4fd
MakeArtKenji <Kennonirom@gmail.com>
2024-10-16 20:05:39 +0800

    change dates to view cert

--- src/components/Work/School.jsx
+++ src/components/Work/School.jsx
@@ -6,19 +6,19 @@ function School() {
         {
             logoSrc: "https://media.designrush.com/inspiration_images/131656/conversions/_1523460223_965_Harvard-Wreath-Logo_ab2ece9fbd52-desktop.jpg",
             title: "CS50x Harvard",
-            duration: "2/23 - 5/23",
+            duration: "View Certificate",
             link: "https://certificates.cs50.io/dab0986a-5bf5-4568-89bf-ce526b8a5cb1.pdf?size=letter"
         },
         {
             logoSrc: "https://media.designrush.com/inspiration_images/131656/conversions/_1523460223_965_Harvard-Wreath-Logo_ab2ece9fbd52-desktop.jpg",
             title: "CS50w Harvard",
-            duration: "12/23 - 6/24",
+            duration: "View Certificate",
             link: "https://certificates.cs50.io/07cf8951-7096-4a72-9245-fcba75963f94.pdf?size=letter"
         },
         {
             logoSrc: "https://freelogopng.com/images/all_img/1683006968udemy-icon-png.png",
             title: "WebDev Udemy",
-            duration: "2/24 - 8/24",
+            duration: "View Certificate",
             link: "https://udemy-certificate.s3.amazonaws.com/image/UC-39cc4dc6-2efe-4bb3-8903-c60993ba2cd7.jpg?v=1724748463000"
         },
         {
